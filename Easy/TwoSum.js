c4540954365d2d6712aff569ff49887e015871f7
Dignesh Solanki <solank86@uwindsor.ca>
2026-03-17 17:41:03 -0400

    DAY - 3

---
+++ Easy/TwoSum.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+
+        for(let i =0; i<nums.length; i++){
+
+            for(let j=0; j<nums.length; j++){
+                if(i == j) continue;
+                if(nums[i]+nums[j]==target){
+                    if(i<=j){
+                        return [i, j];
+                    }
+                    else return [j, i];
+                }
+                else continue;
+            }
+        }
+
+    }
+
+}
