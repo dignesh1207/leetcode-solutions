b21099abdf7f74b1b79789d0cb837b2814ed54df
Dignesh Solanki <solank86@uwindsor.ca>
2026-03-27 16:45:31 -0400

    Better time complexity

--- Medium/Group_Anagrams.py
+++ Medium/Group_Anagrams.py
@@ -21,17 +21,33 @@
 
 # 1 <= strs.length <= 1000.
 # 0 <= strs[i].length <= 100
-# strs[i] is made up of lowercase English letters.
+# strs[i] is made up of lowercase English letters.  there are only 26 unique letters at most 
 
+# time: O(n*k), space: O(n)
+
+# ord -> given a character, return it's ASCII value 
+
+from collections import defaultdict
 
 class Solution:
+    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        groups = {}
+        groups = defaultdict(list) # O(n) -> Space
 
-        for word in strs:
-
-            key = ''.join(sorted(word))
-            groups[key] = groups.get(key, []) + [word]
-
+        for word in strs: # O(n) -> Time
+            key = counter(word) # O(m) -> Time
+            groups[key].append(word) 
+                 
         return list(groups.values())  
+    
+def counter( word: List[str]) -> tuple[int]:
+        result = [0]*26 # O(26) = O(1) -> space
+
+        for ch in word:   # O(m) -> time, m is len(word)
+            result[ord(ch)-ord('a')] += 1
+        
+        return tuple(result)
+
+
+
