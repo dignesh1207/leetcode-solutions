a4851c656b6a132d5f77981a1442623e02918b8e
Dignesh Solanki <solank86@uwindsor.ca>
2026-03-28 01:30:31 -0400

    Optimal solution for 2sum and 3sum with(O^3) TC

--- Easy/TwoSum.js
+++ Easy/TwoSum.js
@@ -1,3 +1,43 @@
+// Two Sum
+// Solved 
+// Easy
+// Topics
+// Company Tags
+// Hints
+// Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+// You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+// Return the answer with the smaller index first.
+
+// Example 1:
+
+// Input: 
+// nums = [3,4,5,6], target = 7
+
+// Output: [0,1]
+// Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+// Example 2:
+
+// Input: nums = [4,5,6], target = 10
+
+// Output: [0,2]
+// Example 3:
+
+// Input: nums = [5,5], target = 10
+
+// Output: [0,1]
+// Constraints:
+
+// 2 <= nums.length <= 1000
+// -10,000,000 <= nums[i] <= 10,000,000
+// -10,000,000 <= target <= 10,000,000
+// Only one valid answer exists.
+
+#Time Complexity: O(n^2)
+#Space Complexity: O(1)
+
 class Solution {
     /**
      * @param {number[]} nums
@@ -23,3 +63,20 @@ class Solution {
     }
 
 }
+
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        hashmap = {}
+
+        for i, x in enumerate(nums):
+            complement = target - x
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[x] = i
+
+
+            
--- Easy/ValidAnagram.js
+++ Easy/ValidAnagram.js
@@ -1,3 +1,31 @@
+// Valid Anagram
+// Solved 
+// Easy
+// Topics
+// Company Tags
+// Hints
+// Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+// Example 1:
+
+// Input: s = "racecar", t = "carrace"
+
+// Output: true
+// Example 2:
+
+// Input: s = "jar", t = "jam"
+
+// Output: false
+// Constraints:
+
+// s and t consist of lowercase English letters.
+
+#Time Complexity: O(n log n) due to sorting
+#Space Complexity: O(n) due to the sorted strings
+
+
 class Solution {
     /**
      * @param {string} s
